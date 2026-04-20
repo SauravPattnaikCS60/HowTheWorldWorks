7f7a53a44855b3eab1f311cf528c577e452b0aca
Saurav Pattnaik <sauravpattnaik@Sauravs-MacBook-Pro.local>
2026-04-20 20:15:20 +0530

    fixed tavily bug

--- tavily_search.py
+++ tavily_search.py
@@ -14,11 +14,6 @@ def parse_results(response):
 
 def call_tavily(query):
     tavily_client = TavilyClient(api_key=os.getenv('TAVILY'))
-    query = "Why is petrodollar so important?"
     response = tavily_client.search(query, search_depth='advanced',max_results=3,chunks_per_source=3)
-    # json.dump(response, open('tavily_response.json','w'))
     response_string = parse_results(response)
     return response_string
-
-if __name__ == "__main__":
-    call_tavily("")
